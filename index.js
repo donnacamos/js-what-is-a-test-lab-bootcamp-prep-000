874570dcf7a38a7a4821e8722ca899a9b11c693d
Donna Amos <donnacamos.developer@gmail.com>
2019-02-05 19:53:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var name = "Joe"
 var name = "Susan"
-var height = 74
+var height => 40
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
